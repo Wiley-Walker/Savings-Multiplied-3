579953992fc64c4d7773ff0697e7c14169d6e913
Nashville Software School <nss@Nashvilles-MacBook-Pro-2.local>
2015-05-29 19:36:38 -0500

    My first commit

---
+++ public/javascripts/item.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+
+  // handler for the PLUS or MINUS icon on each table row
+  // for expanding or contracting content
+  $('.glyphicon-plus-sign').on('click', function() {
+    var item_id = $(this)[0].id;
+    $('.showOrHide_' + item_id).toggle();
+    if ($(this).hasClass("glyphicon-plus-sign")) {
+      $(this).removeClass("glyphicon-plus-sign").addClass("glyphicon-minus-sign");
+    }
+    else {
+      $(this).removeClass("glyphicon-minus-sign").addClass("glyphicon-plus-sign");
+    }
+  });
+
+  // handler for clicking the Edit button
+  $('.edit-btn').on('click', function() {
+    var item_id = $(this)[0].id;
+    window.location.href = '/auction/' + item_id;
+  });
+
+  // handler for clicking the Remove button
+  $('.remove-btn').on('click', function() {
+    var item_id = $(this)[0].id;
+    $('#book_' + item_id).remove();
+
+    $.ajax({
+      url: '/auction',
+      method: 'DELETE',
+      data: {
+        item_id: item_id
+      },
+      success: function(response) {
+        console.log("returned from ajax DELETE");
+      }
+    });
+  });
+
+});
---
+++ public/javascripts/main.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+	var $itemDiv = $('#auction-items');
+	
+	function getAuctionItems() {
+		$.get("/users/getAllItems",
+			function(items) {
+				console.log("after getting all items");
+				//console.log(items);
+				for (var i = 0; i < items.length; i++) {
+					var imageURL = items[i].image;
+					var title = items[i].title;
+					var price = items[i].price;
+					var seller = items[i].user;
+					var endDate = new Date(items[i].endDate);
+					var month = endDate.getMonth() + 1;
+					var day = endDate.getDate();
+					var year = endDate.getFullYear();
+					var $minPrice = $('#js-minPrice').val();
+					var $maxPrice = $('#js-maxPrice').val();
+					
+					if (price >= $minPrice && price <= $maxPrice) {
+						var contentString = "";
+						contentString += "<figure class='filteredItems'>";
+						contentString += "<img class=\"thumb\" src=\"" + imageURL + "\" alt=\"\">";
+						contentString += "<figcaption>" + title + " $" + price.toFixed(2) + "</figcaption>"
+						contentString += "<figcaption>" + seller + " " + month + "-" + day + "-" + year + "</figcaption>"
+						contentString += "</figure>"
+						$itemDiv.before(contentString);
+					}					
+				}
+			});
+		}
+
+$('#seeItems').on('click', function() {
+		$('.filteredItems').remove();
+		getAuctionItems();
+	});
+		
+});
